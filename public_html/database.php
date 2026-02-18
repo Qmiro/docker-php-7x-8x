5278f7600297772096e0ddf3bf4dde41b46a4362
Qmiro <42091234+Qmiro@users.noreply.github.com>
2026-02-18 09:40:00 +0100

    Update database.php
    Test connection database MySql and PostgreSql

--- public_html/database.php
+++ public_html/database.php
@@ -1,1 +1,17 @@
+<?php
+try {
+    $db = new \PDO("pgsql:host=172.24.160.1;port=5432;dbname=development;user=postgres;password=postgres");
+    echo 'Connection PostgreSQL Success!<br/>';
+} catch (\Exception $e) {
+    var_dump($e);
+}
 
+try {
+    $db = new \PDO('mysql:host=172.24.160.1;dbname=development;charset=utf8mb4', 'mysql', 'mysql', array(
+        \PDO::ATTR_EMULATE_PREPARES => false,
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+    ));
+    echo 'Connection MySQL Success!<br/>';
+} catch (\Exception $e) {
+    var_dump($e);
+}
